a7d12f71c500049c213a3d575d577e982b6203da
Alexandre Doussot <alexandre.doussot@epita.fr>
2016-11-09 13:25:22 +0000

    Enable drawing of several entities

--- src/run.cc
+++ src/run.cc
@@ -101,10 +101,9 @@ float* transform(sf::RenderWindow& window, float* coord)
 
 void draw(sf::RenderWindow& window, gl_state& state)
 {
-    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //for (int i = 0; i < 8; i+=2)
-        //state.coord[i] += 1;
+    float pos[] = {20, 20, 80, 10, 300, 400};
 
     glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), state.coord, GL_STATIC_DRAW);
 
@@ -123,29 +122,28 @@ void draw(sf::RenderWindow& window, gl_state& state)
     float xsize = 20;
     float ysize = 20;
 
-    state.xpos += 0.05f;
-    state.counter += 0.05f;
-    state.xpos = fmodf(state.xpos, 800.f);
-    state.ypos = (int)((state.counter + 0.5f) / 800.f) * 20;
+    for (int i = 0; i < 6; i+=2)
+    {
 
-    glm::mat4 mmat; /* Init as identity matrix */
-    mmat = glm::translate(mmat, glm::vec3(state.xpos, state.ypos, 0.0f));
+        glm::mat4 mmat; /* Init as identity matrix */
+        mmat = glm::translate(mmat, glm::vec3(pos[i], pos[i+1], 0.0f));
 
-    /* When there's rotation, shift origin to middle of quad. */
+        /* When there's rotation, shift origin to middle of quad. */
 
-    mmat = glm::translate(mmat, glm::vec3(.5f * xsize, .5f * ysize, 0.f));
-    mmat = glm::rotate(mmat, 0.f, glm::vec3(0.f, 0.f, 1.f));
-    mmat = glm::translate(mmat, glm::vec3(-.5f * xsize, -.5f * ysize, 0.f));
+        mmat = glm::translate(mmat, glm::vec3(.5f * xsize, .5f * ysize, 0.f));
+        mmat = glm::rotate(mmat, 0.f, glm::vec3(0.f, 0.f, 1.f));
+        mmat = glm::translate(mmat, glm::vec3(-.5f * xsize, -.5f * ysize, 0.f));
 
-    mmat = glm::scale(mmat, glm::vec3(xsize, ysize, 1.0f));
+        mmat = glm::scale(mmat, glm::vec3(xsize, ysize, 1.0f));
 
-    GLint transform = glGetUniformLocation(state.prog, "model");
-    glUniformMatrix4fv(transform, 1, GL_FALSE, glm::value_ptr(mmat));
+        GLint transform = glGetUniformLocation(state.prog, "model");
+        glUniformMatrix4fv(transform, 1, GL_FALSE, glm::value_ptr(mmat));
 
-    GLint color = glGetUniformLocation(state.prog, "tri_color");
-    glUniform3f(color, 1.0f, 0.0f, 0.0f);
+        GLint color = glGetUniformLocation(state.prog, "tri_color");
+        glUniform3f(color, 1.0f, 0.0f, 0.0f);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    }
 
     window.display();
 }
