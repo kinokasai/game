084f12206a94dcfb2173190927de3789070b31f1
Alexandre Doussot <alexandre.doussot@epita.fr>
2016-11-09 17:56:54 +0000

    Fix #1

--- src/run.cc
+++ src/run.cc
@@ -1,24 +1,22 @@
-#include <cmath>
-#include <GL/glew.h>
-#include <GL/gl.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/string_cast.hpp>
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics/View.hpp>
-#include <SFML/OpenGL.hpp>
 
-#include "shader.hh"
 #include "render.hh"
 #include "run.hh"
 
 void run(sf::RenderWindow& window)
 {
-    glewExperimental = true;
-    glewInit();
     renderer rendr = init_renderer();
-    while(true)
+    sf::Event event;
+    while (true)
+    {
+        while(window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+                exit(0);
+            }
+
         draw_entities(rendr, window);
+    }
 }
